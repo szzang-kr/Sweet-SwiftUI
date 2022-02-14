06f54120c1924fbd0167e6e6f970586eb3160976
AhnSangHoon <ansh0324@gmail.com>
2022-02-15 00:06:20 +0900

    Initial Xcode project

---
+++ SweetSwiftUI/SweetSwiftUI/SweetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSwiftUIApp.swift
+//  SweetSwiftUI
+//
+//  Created by AhnSangHoon on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct SweetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
