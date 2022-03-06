21b4eed16d376ff3f0dd2288927a98e5f8e40f27
AhnSangHoon <ansh0324@gmail.com>
2022-03-06 15:45:37 +0900

    :building_construction: 프로젝트 구조 변경

---
+++ SweetSwiftUI/SweetSwiftUI/Application/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  SweetSwiftUI
+//
+//  Created by AhnSangHoon on 2022/03/06.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
---
+++ SweetSwiftUI/SweetSwiftUI/Application/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  SweetSwiftUI
+//
+//  Created by AhnSangHoon on 2022/03/06.
+//
+
+import SwiftUI
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    let rootView = ContentView()
+    
+    if let windowScene = scene as? UIWindowScene {
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = UIHostingController(rootView: rootView)
+      self.window = window
+      window.makeKeyAndVisible()
+    }
+  }
+}
